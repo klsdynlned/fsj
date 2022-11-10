d1565b39df254709d85e55c382247ab3cbb5cabf
klsdynlned <113492576+klsdynlned@users.noreply.github.com>
2022-11-10 19:22:36 +0800

    Update show.js

--- show.js
+++ show.js
@@ -1,1 +1,1 @@
-function openwin(url) {\n\nwindow.open (url, \"newwindow\", \"height=372, width=450, top=200,left=200 , toolbar =no, menubar=no, scrollbars=no, resizeable=no, location=no, status=no\")\n\n}
+function openwin(url) {\n\nwindow.open (url, \"newwindow\", \"height=372, width=450, top=200,left=200 , toolbar =no, menubar=no, scrollbars=no, resizeable=no, location=off, status=no\")\n\n}
