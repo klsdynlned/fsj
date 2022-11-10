1ee4a906876045d58d6c0033c310c40420b7e9c8
klsdynlned <113492576+klsdynlned@users.noreply.github.com>
2022-11-10 18:31:14 +0800

    Update 1.js

--- js/1.js
+++ js/1.js
@@ -9,7 +9,7 @@
         //灯泡是开的，把它变成关闭
         img.src="img/开关(2).png";
         	x=document.getElementById("deme");  //查找元素
-			var a="关闭"
+			var a="关闭粉碎"
 			x.innerHTML=a;    //改变内容
 			x.style.color="gray"
         flag = false;
@@ -18,7 +18,7 @@
            img.src="img/开关(1).png";
            
             x=document.getElementById("deme");  //查找元素
-			var a="开启"
+			var a="开始粉碎"
 			x.innerHTML=a;    //改变内容
 			x.style.color="greenyellow"
            flag = true;
