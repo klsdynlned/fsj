7f8f2d16496b832b4d9c68c9b5a6a08110c10d85
klsdynlned <113492576+klsdynlned@users.noreply.github.com>
2022-11-11 14:57:50 +0800

    Update show.js

--- show.js
+++ show.js
@@ -1,1 +1,1 @@
-window.open("index1.html", "newWindow", "width=1024, height=700, top=0, left=0, titlebar=no, menubar=no, scrollbars=yes, resizable=yes, status=yes, , toolbar=no, location=no");
+window.open("index1.html", "newWindow", "width=1024, height=700, top=0, left=0, titlebar=no, menubar=no, scrollbars=no, resizable=no, status=no, , toolbar=no, location=no");
